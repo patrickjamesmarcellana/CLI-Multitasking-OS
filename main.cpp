32ce92669f228a6289daa1a2bd981753fa952df1
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-09-11 22:12:41 +0800

    feat: Add function to get command from user and input validation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
@@ -7,12 +9,21 @@ constexpr auto GREEN_TEXT = "\x1b[38;5;40m";
 constexpr auto LIGHT_YELLOW_TEXT = "\x1b[38;5;229m";
 constexpr auto NORMAL_TEXT = "\x1b[0m";
 
+unordered_set<string> COMMANDS_SET = {
+    "initialize", "screen", "scheduler-test", "scheduler-stop", "report-util", "clear", "exit"
+};
+
 // function prototypes
 void display_header();
+string get_command();
+bool is_valid_command(string);
 
 int main()
 {
     display_header();
+    string command = get_command();
+
+
 }
 
 void display_header() {
@@ -28,4 +39,27 @@ void display_header() {
     cout << GREEN_TEXT << "Hello, Welcome to the CSOPESY command line!" << NORMAL_TEXT << endl;
     cout << LIGHT_YELLOW_TEXT << "Type 'exit' to quit, 'clear' to clear the screen" << NORMAL_TEXT << endl;
     cout << "Enter a command: ";
+}
+
+string get_command() {
+    string command;
+    getline(cin, command);
+
+    if (is_valid_command(command)) {
+        cout << "X command recognized. Doing something." << endl;
+        return command;
+    }
+    else {
+        cout << "X command not recognized." << endl;
+    }
+
+}
+
+bool is_valid_command(string command) {
+    if (COMMANDS_SET.find(command) != COMMANDS_SET.end()) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
