72ee152c575eb7a52fbb4815eabbbc2fae0de2ea
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-09-11 22:09:32 +0800

    feat: Add header messages with custom font colors

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+// constant variables
+constexpr auto GREEN_TEXT = "\x1b[38;5;40m";
+constexpr auto LIGHT_YELLOW_TEXT = "\x1b[38;5;229m";
+constexpr auto NORMAL_TEXT = "\x1b[0m";
+
 // function prototypes
+void display_header();
+
+int main()
+{
+    display_header();
+}
+
 void display_header() {
     cout << R"(
  ______   ______   _______   ______   ______   ______   _     _
@@ -12,9 +24,8 @@ void display_header() {
 | |____   ____| | | |___| | | |      | |____   ____| |    | |
 |______| |______| |_______| |_|      |______| |______|    |_|
     )" << endl;
-}
 
-int main()
-{
-    display_header();
+    cout << GREEN_TEXT << "Hello, Welcome to the CSOPESY command line!" << NORMAL_TEXT << endl;
+    cout << LIGHT_YELLOW_TEXT << "Type 'exit' to quit, 'clear' to clear the screen" << NORMAL_TEXT << endl;
+    cout << "Enter a command: ";
 }
