e76972e2e2c59841005362db080fd568fc4e071b
Harvey Chua <harvs89.2@gmail.com>
2024-09-27 11:22:05 +0800

    feat: initial FCFS impl

--- main.cpp
+++ main.cpp
@@ -1,52 +1,125 @@
-#include <iostream>
+#include <chrono>
+#include <format>
 #include <functional>
-#include <map>
-#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <thread>
+#include <vector>
+#include <memory>
+#include <queue>
+#include <mutex>
 
-namespace shell_commands {
-    typedef std::function<void(std::string)> command_handler;
-    void draw_header() {
-        std::cout << "\033[2J" <<
-            "   ____ ____   ___  ____  _____ ______   __\n" <<
-            "  / ___/ ___| / _ \\|  _ \\| ____/ ___\\ \\ / /\n" <<
-            " | |   \\___ \\| | | | |_) |  _| \\___ \\\\ V / \n" <<
-            " | |___ ___) | |_| |  __/| |___ ___) || |  \n" <<
-            "  \\____|____/ \\___/|_|   |_____|____/ |_|  \n" <<
-            "\n" <<
-            "\033[32mHello, Welcome to CSOPESY commandline!\n" << // 32m is green
-            "\033[93mType 'exit' to quit, 'clear' to clear the screen\n"; // 93m is yellow
+#ifdef _WIN32
+// not a proper fix, sleep_for(1ms) still does not sleep for exactly 1ms i think
+// but it does not sleep for like 10ms anymore
+// better to not sleep at all for small intervals
+// temporary: remove once sleep is no longer used?
+#include <Windows.h>
+#pragma comment(lib, "winmm.lib") // thanks chatgpt
+#endif
+
+using namespace std;
+
+
+
+using ConsoleProgram = function<void(vector<string>)>;
+class Process {
+public:
+    const int pid;
+
+    const std::chrono::time_point<std::chrono::system_clock> time_created;
+    const int lines;
+    int cur_line;
+
+    Process(int pid, int lines) :
+        pid(pid),
+        time_created(std::chrono::system_clock::now()),
+        lines(lines),
+        cur_line(0) {
     }
+};
 
-    void stub(std::string name) {
-        std::cout << name << " command recognized. Doing something." << std::endl;
+namespace emulated_cpu {
+    queue<shared_ptr<Process>> shared_queue;
+    mutex queue_mutex;
+    bool stop_cpus = false;
+    void run_cpu(int cpu_id) {
+        shared_ptr<Process> current_process;
+
+        while (!stop_cpus) { 
+            if (current_process.get() == nullptr || current_process->cur_line >= current_process->lines) {
+                unique_lock queue_lock(queue_mutex);
+
+                if (!shared_queue.empty()) {
+                    current_process = shared_queue.front();
+                    printf("DEBUG: CPU %d took a process\n", cpu_id);
+                    shared_queue.pop();
+                }
+                queue_lock.unlock();
+            }
+            else {
+                if (current_process.get() != nullptr) {
+                    current_process->cur_line++;
+                }
+            }
+            std::this_thread::sleep_for(1ms);
+        }
     }
+};
 
-    const std::map<std::string, command_handler> command_map = {
-        {"clear", [](auto) { draw_header(); }},
-        {"exit",  [](auto) { exit(0); }},
-        {"initialize", stub},
-        {"screen", stub},
-        {"scheduler-test", stub},
-        {"scheduler-stop", stub},
-        {"report-util", stub},
-    };
-}
 
 int main() {
-    shell_commands::draw_header();
-    while (true) {
-        std::string command_string;
+#ifdef _WIN32
+    // improve accuracy of sleep_for
+    timeBeginPeriod(1);
+#endif
+    unordered_map<int, shared_ptr<Process>> processes;
 
-        std::cout << "\033[0mEnter a command: "; // 0m is reset all
-        std::getline(std::cin, command_string);
-        
-        // TODO: split the string and get the first word only (e.g. "screen -ls" -> "screen")
-        auto found_command = shell_commands::command_map.find(command_string); 
-        if (found_command != shell_commands::command_map.end()) {
-            found_command->second(command_string);
+    thread cpu0 = thread(emulated_cpu::run_cpu, 0);
+    thread cpu1 = thread(emulated_cpu::run_cpu, 1);
+
+    ConsoleProgram screen = [&processes](vector<string> params) {
+        enum screen_operation {
+            CREATE,
+            REATTACH,
+        };
+
+        int pid = stoi(params[2]);
+        if (params[1] == "-r") {
+            auto result = processes.find(pid);
+            if (result != processes.end()) {
+                Process* p = result->second.get();
+                cout << "Process Info:" << endl;
+                cout << p->pid << endl;
+                cout << std::format("{}/{}", p->cur_line, p->lines) << endl;
+
+                auto local_time = std::chrono::current_zone()->to_local(p->time_created);
+                cout << std::format("{:%m/%d/%Y %r}", round<chrono::seconds>(local_time)) << endl; // see fmt.dev for syntax
+            }
         }
-        else {
-            std::cout << "Command not recognized." << std::endl;
+        else if (params[1] == "-s") {
+            processes[pid] = make_shared<Process>(pid, 1000);
+
+            unique_lock queue_lock(emulated_cpu::queue_mutex);
+            emulated_cpu::shared_queue.push(processes[pid]);
+            queue_lock.unlock();
         }
-    }
+        };
+
+    ConsoleProgram shell = [&screen](auto) {
+        screen(vector<string>({ "screen", "-s", "11" }));
+        screen(vector<string>({ "screen", "-s", "22" }));
+        screen(vector<string>({ "screen", "-s", "33" }));
+        screen(vector<string>({ "screen", "-s", "44" }));
+
+        screen(vector<string>({ "screen", "-r", "11" }));
+        screen(vector<string>({ "screen", "-r", "22" }));
+        std::this_thread::sleep_for(5000ms);
+        };
+
+    shell({}); // todo: make optional
+
+    emulated_cpu::stop_cpus = true;
+    cpu0.join();
+    cpu1.join();
 }
