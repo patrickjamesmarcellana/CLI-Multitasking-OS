ea375883c62e782fc2488383127bf46c9c471c4e
Harvey Chua <harvs89.2@gmail.com>
2024-10-11 22:21:50 +0800

    feat: implement fine-grained CPU utilization rate

--- CPU.cpp
+++ CPU.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "CPU.h"
+#include <shared_mutex>
 using namespace std::literals::chrono_literals;
 
 
@@ -45,6 +46,11 @@ void CPU::loop() {
             {
                 this->is_busy = false;
             }
+
+            {
+                std::unique_lock lock(lock_cpu_stats);
+                cpu_stats.increment_active();
+            }
         }
 
         sleep(100ms);
@@ -54,6 +60,10 @@ void CPU::loop() {
         // Note, when returning back to ready queue -> set core id of process to -1
     }
 
+    {
+        std::unique_lock lock(lock_cpu_stats);
+        cpu_stats.increment_total();
+    }
     this->inc_cpu_counter();
 }
 
@@ -66,4 +76,9 @@ bool CPU::get_is_busy()
 void CPU::inc_cpu_counter()
 {
     this->process_cpu_counter++;
+}
+
+CPU::cpu_stats_t CPU::get_cpu_stats() {
+    std::shared_lock lock(lock_cpu_stats);
+    return this->cpu_stats;
 }
--- CPU.h
+++ CPU.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <memory>
 #include <queue>
+#include <shared_mutex>
 #include "ConcurrentPtrQueue.h"
 #include "Process.h"
 #include "Worker.h"
@@ -10,6 +11,16 @@ class CPU : Worker {
 public:
     typedef std::shared_ptr<Process> ProcessPtr;
     typedef std::shared_ptr<ConcurrentPtrQueue<Process>>& ProcessQueue;
+    typedef struct {
+        long long int active_counter = 0;
+        long long int total_counter = 0;
+        void increment_active() {
+            active_counter++;
+        }
+        void increment_total() {
+            total_counter++;
+        }
+    } cpu_stats_t;
     enum Algorithm
     {
         FCFS,
@@ -19,6 +30,8 @@ public:
     CPU(int id, Algorithm algorithm, ProcessQueue process_queue, long long int delay_per_exec);
     bool get_is_busy();
     void inc_cpu_counter();
+    void inc_cpu_active_counter();
+    cpu_stats_t get_cpu_stats();
 
 private:
     int id;
@@ -29,4 +42,6 @@ private:
     virtual void loop();
     bool is_busy = false;
     long long int process_cpu_counter = 0;
+    std::shared_mutex lock_cpu_stats;
+    cpu_stats_t cpu_stats = {};
 };
