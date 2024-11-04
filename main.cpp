adb2c9718d0e2a7b2d9f690c96ca5576830a6d12
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-04 17:49:56 +0800

    fix: Fix printing of screen -ls and report-util to keep all columns aligned at all times

--- main.cpp
+++ main.cpp
@@ -166,11 +166,11 @@ Y88b  d88P Y88b  d88P Y88b. .d88P 888        888        Y88b  d88P     888
         stream << "Running processes:" << std::endl;
         for(auto process : global_objects::process_map)
         {
-            if(!process.second->is_done_executing() && process.second->get_assigned_core_id() != -1)
+            if (!process.second->is_done_executing() && process.second->get_assigned_core_id() != -1)
             {
-                stream << process.second->getProcessName() << "\t" << format_time( process.second->get_time_executed()) << "\t\t"
-                << "Core: " << process.second->get_assigned_core_id() << "\t\t" << process.second->getCurrLine() << " / "
-                << process.second->getTotalLines() << std::endl;
+                stream << process.second->getProcessName() << "  \t" << format_time(process.second->get_time_executed()) << "\t "
+                    << "Core: " << process.second->get_assigned_core_id() << " \t" << process.second->getCurrLine() << " / "
+                    << process.second->getTotalLines() << std::endl;
             }
         }
 
